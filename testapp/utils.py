75f3ada7666325d4cfe83e47d8d2fc41efffd69b
David Baumgold <david@davidbaumgold.com>
2017-04-18 09:39:25 -0400

    Use request.Session()

--- testapp/utils.py
+++ testapp/utils.py
@@ -13,16 +13,14 @@ def dropbox_list_folder(user, path=""):
     """
     Returns a generator of files and subfolders for the given folder.
     """
-    social_auth = user.social_auth.filter(provider="dropbox-oauth2").first()
-    if not social_auth:
-        raise ValueError("user is not authorized with Dropbox")
-    headers = {
+    social_auth = user.social_auth.filter(provider="dropbox-oauth2").get()
+    session = requests.Session()
+    session.headers.update({
         "Authorization": "Bearer {token}".format(token=social_auth.access_token),
         "Content-Type": "application/json",
-    }
-    response = requests.post(
+    })
+    response = session.post(
         url="https://api.dropboxapi.com/2/files/list_folder",
-        headers=headers,
         json={"path": path},
     )
     response.raise_for_status()
@@ -31,9 +29,8 @@ def dropbox_list_folder(user, path=""):
         yield undotted_keys(entry)
 
     while content['has_more']:
-        response = requests.post(
+        response = session.post(
             url="https://api.dropboxapi.com/2/files/list_folder/continue",
-            headers=headers,
             json={"cursor": content["cursor"]},
         )
         response.raise_for_status()
